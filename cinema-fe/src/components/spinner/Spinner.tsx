fc7247aea609280b0c5f6a81841f930b704d0b4d
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2024-07-17 17:12:34 +0300

    init: Add Spinner component

---
+++ cinema-fe/src/components/spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import styles from './spinner.module.scss';
+
+const Spinner = () => (
+  <div className={styles.spinner}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Spinner;
--- cinema-fe/src/containers/sections/home-page/cards-lists/GenreSection.tsx
+++ cinema-fe/src/containers/sections/home-page/cards-lists/GenreSection.tsx
@@ -2,28 +2,30 @@ import { useGenres } from '@/hooks/useGenres';
 import Card from '@/components/card/Card';
 import Layout from './Layout';
 import Carousel from '@/components/carousel/Carousel';
+import Spinner from '@/components/spinner/Spinner';
 
 const GenreSection = () => {
   const { isLoading, error, data } = useGenres();
 
-  if (isLoading) return 'Loading...';
-
   if (error) return 'An error has occurred: ' + error.message;
 
   return (
-    <Layout heading="Choose by genres" seeAllHref="/hh">
+    <Layout heading="Choose by genres">
       <Carousel>
         <Card key="filter" href="/filters" title="Filters" />
         <Card key="search" href="/search" title="Search" />
-        {data?.data &&
-          data.data.map((item) => (
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          data?.data?.map((item) => (
             <Card
               key={item.id}
-              href=""
-              title={item.name}
+              href={`/${item.name}`}
+              title={item.description}
               color={item.backgroundGradient}
             />
-          ))}
+          ))
+        )}
       </Carousel>
     </Layout>
   );
