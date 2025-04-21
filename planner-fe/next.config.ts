dedaa93606b1270fd803cf4f0f4ea1945b66ace1
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2025-04-21 21:31:00 +0300

    feat: add vercel.json

--- planner-fe/next.config.ts
+++ planner-fe/next.config.ts
@@ -6,13 +6,13 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
-    NEXT_PUBLIC_APP_SERVER_URL: process.env.NEXT_PUBLIC_APP_SERVER_URL,
+    NEXT_PUBLIC_APP_SERVER: process.env.NEXT_PUBLIC_APP_SERVER_URL,
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4200/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_APP_SERVER_URL}/:path*`,
       },
     ];
   },
