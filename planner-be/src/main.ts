09195e252a38accdb5635e8f2b58a1d7fb87df7d
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2025-05-01 19:59:04 +0300

    build: vercel config

--- planner-be/src/main.ts
+++ planner-be/src/main.ts
@@ -2,16 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
-import { Handler } from 'express';
 
 const server = express();
-const adapter = new ExpressAdapter(server);
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, adapter);
+async function createApp() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
@@ -19,20 +17,23 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: configService.get('FRONTEND_URL') || true,
+    origin: configService.get('FRONTEND_URL'),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.init();
-
-  if (process.env.NODE_ENV === 'development') {
-    const port = configService.get('PORT') || 4200;
-    await app.listen(port);
-    console.log(`Server running on http://localhost:${port}`);
-  }
+  return server;
 }
-bootstrap();
 
-export const handler: Handler = server;
+if (process.env.NODE_ENV === 'development') {
+  createApp().then((app) => {
+    const port = process.env.PORT || 4200;
+    app.listen(port, () => {
+      console.log(`🚀 Server running on http://localhost:${port}`);
+    });
+  });
+}
+
+export default createApp();
