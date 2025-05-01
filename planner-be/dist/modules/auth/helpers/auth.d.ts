41d601e3514a68705fa64a6f26d36f17a5f259e3
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2025-05-01 18:37:58 +0300

    fix: add dist to gitignore

--- planner-be/dist/modules/auth/helpers/auth.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response } from 'express';
-export declare function setAuthCookies(res: Response, accessToken: string, refreshToken: string): void;
-export declare function clearAuthCookies(res: Response): void;
--- planner-be/dist/modules/goals/dto/ManageGoalDto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateGoalDto {
-    title: string;
-    emoji: string;
-    isActive: boolean;
-}
-export declare class UpdateGoalDto extends CreateGoalDto {
-    _id: string;
-}
